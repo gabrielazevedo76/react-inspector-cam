011d795f6dd2f324be946406c0041b485d21199f
Gabriel Almeida <gabrielalmeida75@gmail.com>
2023-07-05 20:29:54 +0000

    chore: criar base para camera

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,67 @@
+import { useState, useRef, useCallback } from "react";
+import Webcam from "react-webcam";
+import { Button } from "@mui/material";
+import styles from "./styles.module.css";
+
 function Home() {
+  const webcamRef = useRef(null);
+  const [imgSrc, setImgSrc] = useState(null);
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+  }, [webcamRef, setImgSrc]);
+
+  const clearCapture = () => {
+    setImgSrc(null);
+  };
+
+  const camView = () => {
+    return (
+      <>
+        <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+        <div className={styles.camOptions}>
+          <Button
+            onClick={capture}
+            variant="contained"
+            className={styles.btnScreenshot}
+          >
+            Foto
+          </Button>
+        </div>
+      </>
+    );
+  };
+
+  const picView = () => {
+    return (
+      <>
+        {imgSrc && <img className={styles.pictureInPicture} src={imgSrc} />}
+        <div className={styles.pictureOptions}>
+          <Button
+            color="success"
+            onClick={clearCapture}
+            variant="contained"
+            className={styles.btnSend}
+          >
+            Voltar
+          </Button>
+          <Button
+            color="success"
+            onClick={clearCapture}
+            variant="contained"
+            className={styles.btnSend}
+          >
+            Enviar
+          </Button>
+        </div>
+      </>
+    );
+  };
+
   return (
     <>
-      <h1>Desenvolvimento de inspecionador usando webcam com react</h1>
+      <div className={styles.container}>{imgSrc ? picView() : camView()}</div>
     </>
   );
 }
